3d22f99f78487be410afd8f6c5c83f50f2bc7edf
Athul <athulsuseelan3@gmail.com>
2025-05-23 10:36:49 +0530

    Add arera.py

---
+++ arera.py
@@ -0,0 +1,10 @@
+length=float(input("Enter the length of the rectangle"))
+breadth=float(input("Enter the breadth of the rectangle"))
+area=float (length*breadth)
+print(f"Area of the rectangle is {area}")
+import math
+radius=float(input("Enter the radius of the circle"))
+area2=float(math.pi*radius*radius)
+cir=float(math.pi*2*radius)
+print(f"The area of the circle is{area2}")
+print(f"The circumferance of the circle is{cir}")
